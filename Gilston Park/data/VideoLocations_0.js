fc3a7daf14ba0dbd8556598ce0fb768d35839976
Vectos Microsimulation Specialists <35485785+vectos-microsim@users.noreply.github.com>
2018-06-22 09:52:43 +0100

    Add files via upload

---
+++ Gilston Park/data/VideoLocations_0.js
@@ -0,0 +1,13 @@
+var json_VideoLocations_0 = {
+"type": "FeatureCollection",
+"name": "VideoLocations_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Location": "4 - Burnt Mill Roundabout (including Central Crossing and Site Access junction)\n", "AM Video": "AM link here", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.090048002459092, 51.782092889421058 ] } },
+{ "type": "Feature", "properties": { "Location": "2 - A1019\/A1025\n", "AM Video": "https:\/\/youtu.be\/4jBjBPUI758", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.097408373713952, 51.767571845844323 ] } },
+{ "type": "Feature", "properties": { "Location": "1 - A1019\/Fourth Avenue\/First Mandela Avenue Junction", "AM Video": "https:\/\/www.youtube.com\/watch?v=gXHcdU5aJc4&feature=youtu.be", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.095335356304409, 51.772934131424861 ] } },
+{ "type": "Feature", "properties": { "Location": "3 - A414\/B183", "AM Video": "AM link here", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.128125003516564, 51.779347499334371 ] } },
+{ "type": "Feature", "properties": { "Location": "5 - Harlow Clock Tower roundabout\n", "AM Video": "AM link here", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.126078900443322, 51.763768610892676 ] } },
+{ "type": "Feature", "properties": { "Location": "6 - Terlings Park access\/Eastern Crossing", "AM Video": "AM link here", "PM Video": "PM link here" }, "geometry": { "type": "Point", "coordinates": [ 0.098830868421208, 51.787080465009829 ] } }
+]
+}
